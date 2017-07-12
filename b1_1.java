9c8f884311605545259c03f7a04a79f13d494bee
NANDHAGOPAL65 <rng650@gmail.com>
2017-07-12 10:18:43 +0530

    Add files via upload

---
+++ b1_1.java
@@ -0,0 +1,28 @@
+package beginner;
+
+import java.util.Scanner;
+
+public class b1_1 
+{
+
+	public static void main(String[] args) 
+	{
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		if(n==0)
+		{
+			System.out.println("Zero");
+		}
+		else if(n>0)
+		{
+			System.out.println("Positive");
+		}
+		else
+		{
+			System.out.println("Negative");
+		}
+		s.close();
+
+	}
+
+}
